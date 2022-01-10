f374fc8385742406d5a1f1211047d9e2ca197b40
Frozen-Fire1 <frosenfire1@gmail.com>
2022-01-09 23:54:55 -0500

    renamed to scrimv3, moved folders around (please don't break the repo)

--- scrimv1 w bugnav and roughly strategized/SageController.java
+++
@@ -1,8 +0,0 @@
-package scrimv1;
-import battlecode.common.*;
-
-public class SageController {
-    static void runSage(RobotController rc) throws GameActionException {
-
-    }
-}
--- scrimv1 w bugnav and roughly strategized/WatchtowerController.java
+++
@@ -1,8 +0,0 @@
-package scrimv1;
-import battlecode.common.*;
-
-public class WatchtowerController {
-    static void runWatchtower(RobotController rc) throws GameActionException {
-
-    }
-}
