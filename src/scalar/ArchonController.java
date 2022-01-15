87b6bb5cae1903a157d8cb4912b022cf5becc1e0
QuestForKnowledge <mastermindmath314@gmail.com>
2022-01-14 21:29:00 -0800

    Minor updated to archoncontroller.java
    Minor edits to account for edge case of nearby enemies (spawn soldiers) or  of spawning soldiers to harvest archon gold

--- src/scalar/ArchonController.java
+++ src/scalar/ArchonController.java
@@ -6,7 +6,7 @@ public class ArchonController {
     static final int MINERS_PER_ARCHON = 12;
     static final int SOLDIERS_PER_ARCHON = 15;
     static int miners = 0, soldiers = 0, builders = 0;
-
+    static int ArchonCurrentHealth;
 
     static void runArchon(RobotController rc) throws GameActionException {
         MapLocation me = rc.getLocation();
@@ -15,6 +15,55 @@ public class ArchonController {
         miners = rc.readSharedArray(0);
         soldiers = rc.readSharedArray(1);
         builders = rc.readSharedArray(2);
+        // If it can see an enemy nearby, panic and produce soldiers
+        //We spawn them in our predetermined spots
+        Direction defensiveSpawn = null;
+
+        //Let's define the north/south direction as our defensive direction
+        if(rc.getMapWidth() - me.y > me.y)
+        {
+            defensiveSpawn = Direction.NORTH;
+        }
+        else
+        {
+            defensiveSpawn = Direction.SOUTH;
+        }
+
+        //Now we spam soldiers if we see them too close
+        while(rc.senseNearbyRobots(-1, rc.getTeam().opponent()) != null)
+        {
+            if (rc.canBuildRobot(RobotType.SAGE, defensiveSpawn))
+            {
+                rc.buildRobot(RobotType.SAGE, defensiveSpawn);
+            }
+
+            else if (rc.canBuildRobot(RobotType.SOLDIER, defensiveSpawn))
+            {
+                rc.buildRobot(RobotType.SOLDIER, defensiveSpawn);
+            }
+        }
+
+        //Archon dying? Harvest that gold by spamming miners!
+
+        //Store old health before updating it
+        int oldArchonCurrentHealth = ArchonCurrentHealth;
+
+        //Update health
+        ArchonCurrentHealth = rc.getHealth();
+
+        int rateOfHealthloss = ArchonCurrentHealth - oldArchonCurrentHealth;
+
+        if (rateOfHealthloss > 100 && rateOfHealthloss > 10)
+        {
+            for(Direction dir : Direction.allDirections())
+            {
+                if (rc.canBuildRobot(RobotType.MINER, dir))
+                {
+                    rc.buildRobot(RobotType.MINER, dir);
+                    rc.setIndicatorString("Damaged and spawning a miner!");
+                }
+            }
+        }
 
         // TODO: erase archon when it dies, can unroll loop for bytecode if needed
         // broadcast our encoded archon locations to the shared arr
