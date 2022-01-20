89bdf59050c79a9ea0d18f62dfc5b36699f6939f
QuestForKnowledge <mastermindmath314@gmail.com>
2022-01-20 01:07:06 -0800

    Update BuilderController.java
    Will theorycraft more; this is a rough outline will not run, braindead. intended 2:1 ratio of watchtowers to labs?

--- src/sad_sprint1_bot/BuilderController.java
+++ src/sad_sprint1_bot/BuilderController.java
@@ -1,10 +1,73 @@
 package sad_sprint1_bot;
-
-import battlecode.common.GameActionException;
-import battlecode.common.RobotController;
+import battlecode.common.*;
+import scalar.util.BFS;
+import scalar.util.DroidBFS;
+import scalar.util.Util;
 
 public class BuilderController {
+    static MapLocation me = null;
+    static int WatchTowerCount = 0;
+    static int LabCount = 0;
+    static int LabCount = 0;
     static void runBuilder(RobotController rc) throws GameActionException {
+        me = rc.getLocation();
+
+
+        //TODO: BFS to a nearby location first
+        int distToNorthWall = rc.getMapHeight() - me.y;
+        int distToSouthWall = me.y;
+
+        int distToEastWall = rc.getMapWidth() - me.x;
+        int distToWestWall = me.x;
+
+        int yCoordmin = Math.min(distToSouthWall, (int) (0.3 * rc.getMapHeight());
+        int xCoordmin = Math.min(distToWestWall, (int) (0.3 * rc.getMapWidth());
+        int yCoordmax = Math.min(distToNorthWall, (int) (0.3 * rc.getMapHeight());
+        int xCoordmax = Math.min(distToEastWall, (int) (0.3 * rc.getMapWidth());
+
+        int xCoordRandom = Util.rng.nextInt(me.x - xCoordmin, me.x-xCoordmax);
+        int yCoordRandom = Util.rng.nextInt(me.y - yCoordmin, me.y-yCoordmax);
+
+        MapLocation target = new MapLocation(xCoordRandom, yCoordRandom);
+
+        //TODO: BFS to this location. Add a check that its at least 5 squares from the Archon at nearly all times
+
+
+        //Build!
+        //TODO: Build randomly
+        if (safeBuild(RobotType.WATCHTOWER, dir) && (WatchTowerCount + LabCount == 1%3);
+        {
+            rc.buildRobot(RobotType.WATCHTOWER, dir);
+            WatchTowerCount++;
+        }
+        else if (safeBuild(RobotType.LABORATORY, dir))
+        {
+            rc.buildRobot(RobotType.LABORATORY, dir);
+            LabCount++;
+        }
+
+        //Now, repair anything nearby.
+
+        RobotInfo [] ListofNearbyRobots = rc.senseNearbyRobots(-1, rc.getTeam());
+        //Check if any of those robots are repairable and if we can repair the same watchtower multiple times
+        for (RobotInfo Robot : ListofNearbyRobots)
+        {
+            if (rc.canRepair(Robot.getLocation()))
+            {
+                rc.repair(Robot.getLocation());
+            }
+
+        }
+
+        //Mutate watchtowers only to level 2
+        for (RobotInfo Robot : ListofNearbyRobots)
+        {
+            if (rc.canMutate(Robot.getLocation()) && (rc.getType() == RobotType.WATCHTOWER || rc.getType() == RobotType.LABORATORY) && rc.getLevel() == 1)
+            {
+                rc.mutate(Robot.getLocation());
+            }
+
+        }
 
     }
 }
