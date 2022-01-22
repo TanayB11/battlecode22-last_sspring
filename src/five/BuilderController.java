c0287a7f160037592ad02b0fb70e63d8137e32fb
QuestForKnowledge <mastermindmath314@gmail.com>
2022-01-21 18:38:32 -0800

    Update BuilderController.java
    in progress builder (only accounts for first total builders that only do labs), do not use

--- src/five/BuilderController.java
+++ src/five/BuilderController.java
@@ -7,23 +7,47 @@ import five.util.pathfinding.BFS;
 import five.util.pathfinding.DroidBFS;
 
 public class BuilderController {
-    static MapLocation me = null;
-    static int BuilderCount = 0;
+    static MapLocation Archon = null;
     static BFS bfs = null;
     static boolean wantMoreLabs = false;
+    static MapLocation target = null;
+    static MapLocation homeArchonLoc = null;
 
     static void runBuilder(RobotController rc) throws GameActionException {
         BuilderCount++;
 
+
         //TODO: Get Archon Location
         me = ;
 
+        //Send robot ID to the comms array for unique identification.
+
+        //TODO: replace placeholder:
+        int RobotID;
+        RobotInfo[] nearbyArchon = rc.senseNearbyRobots(rc.getLocation(), -1, rc.getTeam());
+
+        //Get Archon Location if one is not already existed
+        if (homeArchonLoc.equals(null)) {
+
+            for (RobotInfo Robot: nearbyArchon) {
+
+                if (Robot.getType() == RobotType.ARCHON) {
+
+                    homeArchonLoc = new MapLocation(Robot.getLocation().x, Robot.getLocation().y);
+                }
+            }
+        }
+
         //Assumes we're only making labs so far (watchtowers later)
-        // 1 = Archon should save for a single lab, else not.
+
         wantMoreLabs = true;
 
         if (rc.readSharedArray(10) == 1)
         {
+
+        //1 = Archon should save for a single lab, else not.
+
+        if (rc.readSharedArray(10) == 1) {
 
         }
 
@@ -34,19 +58,24 @@ public class BuilderController {
         //Decide where we want them to move towards.
         //Below is for builders that take care of labs
 
-        int distToNorthWall = rc.getMapHeight() - me.y;
-        int distToSouthWall = me.y;
-        int distToEastWall = rc.getMapWidth() - me.x;
-        int distToWestWall = me.x;
+        int distToNorthWall = rc.getMapHeight() - homeArchonLoc.y;
+        int distToSouthWall = homeArchonLoc.y;
+        int distToEastWall = rc.getMapWidth() - homeArchonLoc.x;
+        int distToWestWall = homeArchonLoc.x;
 
         //TODO: fix inefficiency
+
         MapLocation dirForFirstLab = null;
         MapLocation dirForSecondLab = null;
+
+       /*  MapLocation LocForFirstLab = null;
+        MapLocation LocForSecondLab = null;
 
         MapLocation dirLabPriorityOne = null;
         MapLocation dirLabPriorityTwo = null;
 
         MapLocation PriorityOneAdjusted = null;
+
         MapLocation PriorityTwoAdjusted = null;
         //Based on this, we decide on the two walls to check out
         //Edge cases: Archon against a wall or corner
@@ -84,33 +113,93 @@ public class BuilderController {
 
 
         //First Builder goes to whichever target is closest
+=======
+        MapLocation PriorityTwoAdjusted = null; */
+
+        //Based on this, we decide on the two walls to check out
+        //Edge cases: Archon against a wall or corner
+
+        //First lab location if one hasn't already been set yet and this is Builder #1
+         if (RobotID == 1 && target == null){
+
+            if (distToNorthWall >= distToSouthWall && distToSouthWall > 0) {
+                target = new MapLocation (homeArchonLoc.x, 0);
+            }
+            else if (distToSouthWall >= distToNorthWall && distToNorthWall > 0) {
+                target = new MapLocation (homeArchonLoc.x, rc.getMapHeight());
+            }
+        }
+        //Second lab location if one hasn't already been set yet and this is Builder #2
+        else if (RobotID == 2 && target == null) {
+
+             if (distToWestWall >= distToEastWall && distToEastWall > 0) {
+                 target = new MapLocation(rc.getMapWidth(), homeArchonLoc.y);
+             }
+             else if (distToEastWall >= distToWestWall && distToWestWall > 0) {
+                 target = new MapLocation(0, homeArchonLoc.y);
+             }
+         }
 
         //Initialize and move to priority spot
-        //TODO: actually search the spot to make sure its not high rubble;
+        //TODO: actually search the spot once there to make sure its not high rubble;
         //Adjusted terms will be modified
         //set goals to whatever our adjusted location is
 
         bfs = new DroidBFS(rc);
+
         Direction dirAdjustedLab = null;
 
         if (BuilderCount == 1 && wantMoreLabs){
             bfs.move(dirLabPriorityOne);
             //search around once in area
 
-            if (rc.getLocation() == dirLabPriorityOne) {
+        boolean isEnemiesNear;
+
+        RobotInfo[] nearbyEnemies = rc.senseNearbyRobots(-1, rc.getTeam().opponent());
+
+        if (RobotID == 1 && wantMoreLabs){
+                bfs.move(target);
+            }
+
+            if (rc.getLocation().equals(target)) {
                 //Once we're there, check the area for a new target with lower rubble
 
             }
         }
+
         //dirAdjustedLab = rc.getLocation().directionTo().PriorityOneAdjusted.opposite();
         else if (BuilderCount == 2 && wantMoreLabs){
             bfs.move(dirLabPriorityTwo);
             //search around once in area
+
+        if (BuilderCount == 2 && wantMoreLabs){
+        bfs.move(target);
+        //search around once in area
+    }
+
+            if (rc.getLocation().equals(target)) {
+        //Once we're there, check the area for a new target with lower rubble
+
+        if (nearbyEnemies != null)
+        {
+            isEnemiesNear = true;
+            //TODO: Send a flag
+        }
+        else
+        {
+            isEnemiesNear = false;
+
         }
         //dirAdjustedLab = rc.getLocation().directionTo().PriorityOneAdjusted.opposite();
 
 
         if ()
+
+    }
+        //get opposite direction so we don't build off the map
+    //TODO: adjust diAdjustedLab to account for rubble
+        dirAdjustedLab = Archon.directionTo().target.opposite();
+
 
         if (rc.getLocation() == safeBuild(rc, RobotType.LABORATORY, dirAdjustedLab) && wantMoreLabs);
         {
@@ -119,6 +208,7 @@ public class BuilderController {
 
         //Repair labs only
         RobotInfo [] ListofNearbyRobots = rc.senseNearbyRobots(-1, rc.getTeam());
+
         //Check if any of those robots are repairable and if we can repair the same lab multiple times
         for (RobotInfo Robot : ListofNearbyRobots)
         {
@@ -140,4 +230,4 @@ public class BuilderController {
         }*/
 
     }
-}
+
